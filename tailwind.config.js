9d7e0f591deb2258265e7238161a7b0e6a820c39
Thawatchai <thawatchai.krai@gmail.com>
2022-11-29 20:09:13 +0700

    ✨ Add: design system color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,64 +8,80 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        black: {
-          100: "#8B8B8B",
-          200: "#595959",
-          300: "#363636",
-          400: "#183153",
-          500: "#000000"
+        primary: {
+          100: "#C8FACD",
+          200: "#5BE584",
+          300: "#00AB55",
+          400: "#007B55",
+          500: "#005249"
         },
-        white: {
-          100: "#FFFFFF",
-          200: "#F8F8F8",
-          300: "#F5F5F5",
+        secondary: {
+          100: "#D6E4FF",
+          200: "#84A9FF",
+          300: "#3366FF",
+          400: "#1939B7",
+          500: "#091A7A"
         },
         grey: {
-          100: "#E7E7E7",
-          200: "#DBDBDB",
-          300: "#A9A9A9",
+          100: "#F9FAFB",
+          200: "#F4F6F8",
+          300: "#DFE3E8",
+          400: "#C4CDD5",
+          500: "#919EAB",
+          600: "#637381",
+          700: "#454F5B",
+          800: "#212B36",
+          900: "#161C24"
         },
-        primary: {
-          100: "#FDCBBD",
-          200: "#F04E23",
-          300: "#D4421A",
+        info: {
+          100: "#CAFDF5",
+          200: "#61F3F3",
+          300: "#00B8D9",
+          400: "#006C9C",
+          500: "#003768"
         },
-        orange: {
-          100: "#FCCE87",
-          200: "#F59120",
-          300: "#E8741A",
+        success: {
+          100: "#D8FBDE",
+          200: "#86E8AB",
+          300: "#36B37E",
+          400: "#1B806A",
+          500: "#0A5554"
         },
-        yellow: {
-          100: "#FFE288",
-          200: "#FFC627",
-          300: "#FE951B",
+        warning: {
+          100: "#FFF5CC",
+          200: "#FFD666",
+          300: "#FFAB00",
+          400: "#B76E00",
+          500: "#7A4100"
         },
-        green: {
-          100: "#9ED498",
-          200: "#2A9C21",
-          300: "#047A05",
+        error: {
+          100: "#FFE9D5",
+          200: "#FFAC82",
+          300: "#FF5630",
+          400: "#B71D18",
+          500: "#7A0916"
         },
-        red: {
-          100: "#F89791",
-          200: "#D10E0E",
-          300: "#C30000",
+      },
+      keyframes: {
+        'ripple-effect': {
+          '0%': { transform: 'scale(1)', opacity: 1 },
+          '50%': { transform: 'scale(10)', opacity: 0.375 },
+          '100%': { transform: 'scale(35)', opacity: 0 },
         },
-        blue: {
-          100: "#A2EBEB",
-          200: "#D10E0E",
-          300: "#C30000",
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
         },
-        purple: {
-          100: "#B39DDC",
-          200: "#673BB8",
-          300: "#512EA9",
-        },
-        pink: {
-          100: "#B39DDC",
-          200: "#673BB8",
-          300: "#512EA9",
-        },
-
+      },
+      animation: {
+        'ripple': '1.2s ease 1 forwards ripple-effect',
+        'waving-hand': 'wave 2s linear infinite',
       },
     },
   },
