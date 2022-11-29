c7ae4b946eedaab44b8ae6ce1768179617e81e23
Thawatchai <thawatchai.krai@gmail.com>
2022-11-29 20:12:10 +0700

    💫 Update: ripple effect button

--- core/components/Buttons/Button.tsx
+++ core/components/Buttons/Button.tsx
@@ -1,24 +1,48 @@
-import React, { ComponentProps } from 'react'
+import React, { ReactNode } from 'react'
+import useRipple from '@core/hooks/useRipple'
 
-interface ButtonProps extends Omit<ComponentProps<'button'>, 'isLoading'> {
+interface ButtonProps {
+  color?: 'primary' | 'secondary'
+  size?: 'small' | 'medium' | 'large'
   isLoading?: boolean
+  children?: ReactNode
+  className?: string
+  onClick: Function
 }
 
-const Button = ({ isLoading, children, className, ...props }: ButtonProps) => {
+const colorStyle = {
+  primary: `bg-primary-300 text-white hover:bg-primary-400`,
+  secondary: `bg-secondary-300 text-white hover:bg-secondary-400`
+}
+
+const sizeStyle = {
+  small: `px-2.5 py-1`,
+  medium: `px-4 py-1.5`,
+  large: `px-6 py-2`
+}
+
+export const Button = ({ color = 'primary', size = 'medium', isLoading, children, className, onClick, ...props }: ButtonProps) => {
+  const { coords, setCoords, isRippling } = useRipple()
+
   return (
     <button
-      className={`rounded-lg border-none bg-primary-200 inline-block px-10 text-center py-2 heading7 text-white-100 hover:bg-primary-300 ${className}`}
+      className={`relative rounded-lg ripple-button border-none inline-block text-center py-1.5 heading7 text-white-100 overflow-hidden ${className} ${colorStyle[color]} ${sizeStyle[size]}`}
+      onClick={(e: any) => {
+        const position = e?.target?.getBoundingClientRect()
+        setCoords({ x: e.clientX - position.left, y: e.clientY - position.top })
+        onClick && onClick(e)
+      }}
       {...props}>
-      {!isLoading ? (
-        <p>
-          Loading...
-          <i className="ml-2 fa-solid fa-spinner animate-spin" />
-        </p>
-      ) : (
-        children
-      )}
+      {isRippling ? (
+        <span
+          className="w-6 h-6 absolute block bg-white/60 rounded-full after:content=[''] opacity-100 animate-ripple"
+          style={{
+            left: coords.x,
+            top: coords.y
+          }}
+        />
+      ) : null}
+      {isLoading ? <i className="fa-solid fa-spinner animate-spin" /> : <div className="relative z-20">{children}</div>}
     </button>
   )
 }
-
-export default Button
