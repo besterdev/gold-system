f17cd766079efd20ba84e632f960a16119f4a7f5
Thawatchai <thawatchai.krai@gmail.com>
2022-12-06 21:14:24 +0700

    ✨ Update: text input component

--- core/components/Forms/TextInput.tsx
+++ core/components/Forms/TextInput.tsx
@@ -1,42 +1,47 @@
 import { forwardRef, ComponentProps } from 'react'
 
 export interface TextInputProps extends Omit<ComponentProps<'input'>, 'ref' | 'color'> {
+  id: string
   label?: string
   error?: string
 }
 
 const inputStyles = {
-  default: `focus:border-primary-200 focus:ring-primary-200`,
-  error: `focus:border-red-200 focus:ring-red-200 border-red-200`
+  default: `border-grey-300  hover:border-grey-800 `,
+  error: `border-error-300`
 }
 
 const labelStyles = {
-  default: `text-black-200`,
-  error: `text-red-300`
+  default: `text-grey-400 peer-focus:text-grey-800`,
+  error: `text-error-300 peer-focus:text-error-300`
 }
 
 // eslint-disable-next-line react/display-name
-const TextInput = forwardRef<HTMLInputElement, TextInputProps>(({ label, error, className, ...props }, ref) => {
+export const TextInput = forwardRef<HTMLInputElement, TextInputProps>(({ id, label, error, className, ...props }, ref) => {
   return (
-    <div className={`flex flex-col ${className}`}>
-      <label htmlFor={label} className={`heading6 mb-2 text-black-400 ${error ? labelStyles.error : labelStyles.default}`}>
-        {label}
-      </label>
+    <div className={`relative ${className}`}>
       <input
-        {...props}
+        type="text"
+        id={id}
+        className={`text-black-500 subtitle2 peer h-14 w-full appearance-none rounded-md border px-4 placeholder-transparent shadow-sm
+           ring-0 focus:right-0 focus:outline-none
+             disabled:cursor-not-allowed 
+            disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none ${
+              error ? inputStyles.error : inputStyles.default
+            }`}
+        placeholder=" "
         ref={ref}
-        className={`heading6 disabled:shadow-none" h-10 rounded-md border-2 px-4 text-black-500 
-                  placeholder-grey-200 shadow-sm 
-                    focus:outline-none focus:ring-1 
-                    disabled:cursor-not-allowed disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 ${
-                      error ? inputStyles.error : inputStyles.default
-                    }`}
+        {...props}
       />
-      <label htmlFor={error} className="body1 mt-2 text-red-200">
-        {error}
+      <label
+        htmlFor={id}
+        className={`body1 absolute top-1.5 left-2 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-auto bg-white px-1  duration-300 
+        peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1.5 peer-focus:-translate-y-4 
+        peer-focus:scale-75 peer-focus:px-1
+        ${error ? labelStyles.error : labelStyles.default}
+        `}>
+        {label}
       </label>
     </div>
   )
 })
-
-export default TextInput
